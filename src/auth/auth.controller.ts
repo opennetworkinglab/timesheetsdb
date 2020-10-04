f2ea2c3932203ae9c05cda111a8a92801ebe912b
Valdar <valdar@opennetworking.org>
2020-10-04 17:59:24 +0100

    Moved credentails for google to .env. Created restpoint to generate google token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { Body, Controller, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UpdateResult } from 'typeorm';
 import { EmailValidationPipe } from './pipes/email-validation.pipe';
@@ -23,11 +23,14 @@ import { GetUser } from './get-user.decorator';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { gDriveAuth } from '../gdrive/gdrive-auth';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,
+              private configService: ConfigService) {}
 
   @Post('createuser')
   @UseGuards(AuthGuard())
@@ -60,5 +63,30 @@ export class AuthController {
   @UseGuards(AuthGuard())
   getUsers(@GetUser() user: User): Promise<User[]> {
     return this.authService.getUsers(user);
+  }
+
+  @Get('google/auth/token')
+  @UseGuards(AuthGuard())
+  generateGoogleAuthToken(@GetUser() user: User){
+
+    if(!user.isSupervisor){
+      throw new UnauthorizedException("Not authorized");
+    }
+
+    const redirectUris = this.configService.get<string>('GOOGLE_REDIRECT_URIS').split(', ');
+
+    const credentials= {
+      "installed": {
+        "client_id": this.configService.get<string>('GOOGLE_CLIENT_ID'),
+        "project_id": this.configService.get<string>('GOOGLE_PROJECT_ID'),
+        "auth_uri": this.configService.get<string>('GOOGLE_AUTH_URI'),
+        "token_uri": this.configService.get<string>('GOOGLE_TOKEN_URI'),
+        "auth_provider_x509_cert_url": this.configService.get<string>('GOOGLE_AUTH_PROVIDER_X509_CERT_URL'),
+        "client_secret": this.configService.get<string>('GOOGLE_CLIENT_SECRET'),
+        "redirect_uris": redirectUris
+      }
+    }
+
+    gDriveAuth.generateToken(credentials);
   }
 }
