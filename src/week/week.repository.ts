c06f4098008502c77700037a2503de944e03d53e
ValdarRudman <43271709+ValdarRudman@users.noreply.github.com>
2020-12-11 16:45:16 +0000

    Fixed weekNo start from 1 for new year. Added rest point. Will add 52 new weeks when called (#72)
    Co-authored-by: Valdar <valdar@opennetworking.org>

--- src/week/week.repository.ts
+++ src/week/week.repository.ts
@@ -44,20 +44,62 @@ export class WeekRepository extends Repository<Week> {
     return await this.findOne({ id: id });
   }
 
+  async createWeeks(): Promise<void> {
+
+    const lastWeek = await this.findOne({
+      order: { id: 'DESC' }
+    });
+
+    const currentDate = new Date(lastWeek.begin);
+
+    let currentYear = currentDate.getFullYear();
+
+    const weekS = 168 * 60 * 60 * 1000;
+    const startDate = Math.round(+currentDate);
+    let initWeek = lastWeek.weekNo + 1;
+
+    for (let w = 1; w <= 52; w++) {
+
+      const d = new Date(startDate + w * weekS);
+
+      if(d.getFullYear() !== currentYear){
+        initWeek = 1;
+        currentYear = d.getFullYear();
+      }
+
+      const week = new Week();
+      week.year = d.getFullYear();
+      week.weekNo = initWeek;
+      week.monthNo = d.getMonth() + 1;
+      week.begin = new Date(d.getTime());
+      week.end = new Date(d.getTime() + (weekS) - 1000);
+      week.onfDays = [];
+      await week.save();
+      initWeek++;
+    }
+  }
+
   async createWeek(): Promise<void> {
 
     const weekMs = 168 * 60 * 60 * 1000;
     const startDate = 1588550400000; // Mon 4th may 2020
-    const initWeek = 18;
+    let initWeek = 19;
+
+    let currentYear = new Date(startDate + 1 * weekMs).getFullYear();
 
     // Populating weeks table 34
     for (let w = 1; w <= 52; w++) {
 
       const d = new Date(startDate + w * weekMs);
 
+      if(d.getFullYear() !== currentYear){
+        initWeek = 1;
+        currentYear = d.getFullYear();
+      }
+
       const week = new Week();
       week.year = d.getFullYear();
-      week.weekNo = initWeek + w;
+      week.weekNo = initWeek;
       week.monthNo = d.getMonth() + 1; // Its from 0 - 11
       week.begin = new Date(d.getTime());
       week.end = new Date(d.getTime() + weekMs - 1000);
@@ -76,6 +118,7 @@ export class WeekRepository extends Repository<Week> {
       }
 
       await week.save();
+      initWeek++;
     }
   }
 }
